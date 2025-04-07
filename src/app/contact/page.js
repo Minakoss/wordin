2ae05cc48e422964c6108016d986f8494e64a3bc
Minakoss <nontaspapastefanis@gmail.com>
2025-04-07 19:42:22 +0300

    fix submit button

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -118,7 +118,7 @@ export default function Contacts() {
                   status === "sending" ? "opacity-50 cursor-not-allowed" : ""
                 }`}
               >
-                {status === "sending" ? "Sending..." : "Submit"}{" "}
+                {status === "sending" ? "Sending..." : "Αποστολη"}{" "}
                 <span className="ml-2">→</span>
               </button>
             </div>
